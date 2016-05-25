7c4a9077810a8c6fb0277f299f539b157e90e63d
Vasin W <vasinwr@gmail.com>
2016-05-25 19:02:00 +0100

    different lecturer/student view in one index template

--- slides_app/slides/urls.py
+++ slides_app/slides/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = 'slides'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^next_page/', views.next_page, name='next page'),
-    url(r'^prev_page/', views.prev_page, name='previous page'),
+    url(r'^index_lecturer/', views.lecturer, name='index lecturer'),
+    url(r'^index_student/', views.student, name='index student'),
+    url(r'^lecture/next_page/', views.next_page, name='next page'),
+    url(r'^lecture/prev_page/', views.prev_page, name='previous page'),
+    #lecture/ must be at the bottom otherwise it will always be matched first
+    url(r'^lecture/', views.lecture, name='lecture'),
 ]
--- slides_app/slides/views.py
+++ slides_app/slides/views.py
@@ -7,7 +7,15 @@ from .models import Current, Slides
 # Create your views here.
 
 def index(request):
-    
+    return render(request, 'slides/index.html') 
+
+def lecturer(request):
+    return lecture(request, True)
+
+def student(request):
+    return lecture(request, False)
+
+def lecture(request, isLecturer):
     current = get_object_or_404(Current, pk=1)
     '''
     qs = Slides.objects.filter(slide_text=current.slide_name)
@@ -16,20 +24,21 @@ def index(request):
     return render(request, 'slides/index.html', {'slide': slide})
     '''
     slide = get_object_or_404(Slides, pk=current.page)
-    return render(request, 'slides/index.html', {'slide':slide})
-'''
-def index(request):
-    return HttpResponse("Hello, you are at the slides index.")
-'''
+    if(isLecturer):
+        return render(request, 'slides/lecture.html', {'slide':slide, 'lecturer':True})
+    else:
+        return render(request, 'slides/lecture.html', {'slide':slide, 'student':True})
 
 def next_page(request):
     current = get_object_or_404(Current, pk=1)
     current.page += 1
     current.save()
-    return HttpResponseRedirect(reverse('slides:index'))
+    return lecture(request, True)
+#    return HttpResponseRedirect(reverse('slides:lecture', args=(True,)))
 
 def prev_page(request):
     current = get_object_or_404(Current, pk=1)
     current.page -= 1
     current.save()
-    return HttpResponseRedirect(reverse('slides:index'))
+    return lecture(request, True)
+#    return HttpResponseRedirect(reverse('slides:lecture', args=(True,)))
