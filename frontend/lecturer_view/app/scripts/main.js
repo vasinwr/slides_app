d057b8d68775935ecb5b4c4e1398d610fbe4b19d
Vasin W <vasinwr@gmail.com>
2016-06-08 11:29:45 +0100

    fixed build bug, now really works, added dummy mainmenu

--- frontend/lecturer_view/app/scripts/main.js
+++ frontend/lecturer_view/app/scripts/main.js
@@ -3,7 +3,13 @@ Mousetrap.bind(['right','space'], function() { alert('next page') });
 Mousetrap.bind('left', function() { alert('previous page') });
 
 var app = angular.module('lecture',[]);
-app.controller('MenuController', function(){
+
+app.factory('SharedData', function(){
+  return { mainmenuHide: false };
+});
+
+app.controller('MenuController', function(SharedData){
+  this.SharedData = SharedData;
   this.hover = false; 
   this.clicked = false;
   this.summ_hover  =false ;
@@ -16,15 +22,29 @@ app.controller('MenuController', function(){
                {question: 'what happened to our coke machine', votes: 3}]
 });
 
-app.directive('menuList', function(){
-  return{
-    restrict: 'E',
-    templateUrl: 'ng_template/menu-list.html'
-  };
-});
+
 
 
 app.controller('SidesController', function(){
   this.left_hover = false; 
   this.right_hover = false;
+});
+
+app.controller('MainmenuController', function(SharedData){
+  this.SharedData = SharedData;
+  this.hide = false; 
+  this.xs = [1,2,3,4,5];
+  this.icon_hover = false;
+});
+
+
+
+
+
+
+app.directive('menuList', function(){
+  return{
+    restrict: 'E',
+    templateUrl: 'ng_template/menu-list.html'
+  };
 });
